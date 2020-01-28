10b5993db27e904d730a3a3ad64d09683486990a
vsn4ke <vsn4ke@hotmail.fr>
2020-01-28 09:58:30 +0100

    CSS + table

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
   $APIdata = json_decode(file_get_contents("https://poe.ninja/api/data/currencyoverview?league=Metamorph&type=Currency&language=en", true));
   $display = "";
   foreach($APIdata->lines as $key=>$data){
-    $display .= "    <tr>\n"; 
-    $display .= "      <td>". $data->currencyTypeName . "</td>\n";
-    $display .= "      <td>". $data->chaosEquivalent . "</td>\n";
-    $display .= "    </tr>\n"; 
+    $display .= "        <tr>\n"; 
+    $display .= "          <td>". $data->currencyTypeName . "</td>\n";
+    $display .= "          <td class=\"right\">". $data->chaosEquivalent . "</td>\n";
+    $display .= "        </tr>\n"; 
   }
 ?>
 <!doctype html>
@@ -22,50 +22,60 @@
 
 </head>
 <body>
-  <table>
-
-  </table>
-  <div id="calculator">
-    <div class="column1">
-      <div class="displayOutput">
+  <div class="wrapper">
+    <div id="calculator">
+      <div class="column1">
+        <div class="displayOutput">
+        
+        </div>
+        <div class="firstLine">
+            <button type="button" name="C">C</button> 
+            <button type="button" name="/">/</button>
+            <button type="button" name="*">*</button>
+            <button type="button" name="-">-</button>
+        </div>
+        <div class="buttonBlock">
+          <div class="numberBlock">
+            <div class="buttonLine">
+              <button type="button" name="7">7</button>
+              <button type="button" name="8">8</button>
+              <button type="button" name="9">9</button>
+            </div>
+            <div class="buttonLine">
+              <button type="button" name="4">4</button>
+              <button type="button" name="5">5</button>
+              <button type="button" name="6">6</button>
+            </div>
+            <div class="buttonLine">
+              <button type="button" name="1">1</button>
+              <button type="button" name="2">2</button>
+              <button type="button" name="3">3</button>
+            </div>
+            <div class="buttonLine">
+              <button type="button" name="0" class="wide">0</button>
+              <button type="button" name=".">.</button>
+            </div>
+          </div>
+          <div class="sideButton">
+            <button type="button" name="+" class="tall">+</button>
+            <button type="button" name="=" class="tall">=</button>
+          </div>
+        </div>
+      </div>
+      <div class="column2">
       
       </div>
-      <div class="firstLine">
-          <button type="button" name="C">C</button> 
-          <button type="button" name="/">/</button>
-          <button type="button" name="*">*</button>
-          <button type="button" name="-">-</button>
-      </div>
-      <div class="buttonBlock">
-        <div class="numberBlock">
-          <div class="buttonLine">
-            <button type="button" name="7">7</button>
-            <button type="button" name="8">8</button>
-            <button type="button" name="9">9</button>
-          </div>
-          <div class="buttonLine">
-            <button type="button" name="4">4</button>
-            <button type="button" name="5">5</button>
-            <button type="button" name="6">6</button>
-          </div>
-          <div class="buttonLine">
-            <button type="button" name="1">1</button>
-            <button type="button" name="2">2</button>
-            <button type="button" name="3">3</button>
-          </div>
-          <div class="buttonLine">
-            <button type="button" name="0" class="wide">0</button>
-            <button type="button" name=".">.</button>
-          </div>
-        </div>
-        <div class="sideButton">
-          <button type="button" name="+" class="tall">+</button>
-          <button type="button" name="=" class="tall">=</button>
-        </div>
-      </div>
     </div>
-    <div class="column2">
-    
+    <div class="table">
+      <table>
+        <thead>
+          <tr>
+            <td>Currency</td>
+            <td>Value in Chaos</td>
+          </tr>
+        </thead>
+<?=$display?>
+      </table>
     </div>
   </div>
 </body>
